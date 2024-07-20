097f20f5a637cf47e800c144c5e0c0e7450aea8d
Nelson Neto <nhsneto@gmail.com>
2024-07-20 02:20:38 -0300

    Create main content

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,28 @@
 import Navbar from "@/app/components/Navbar";
+import { montserrat } from "@/app/fonts";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div className="container">
       <Navbar />
+
+      <main className={styles.main}>
+        <div className={styles.container}>
+          <div>
+            <h1 className={styles.title}>Nelson Neto</h1>
+            <p className={`${styles.subtitle} ${montserrat.className}`}>
+              Software Developer
+            </p>
+          </div>
+          <p className={`${styles.about} ${montserrat.className}`}>
+            Hi, I&apos;m a junior software developer based in Pernambuco
+            (Brazil), and a software development student at <abbr>IFPE</abbr>{" "}
+            (Instituto Federal de Pernambuco). I&apos;ve developed some projects
+            using <em>PHP</em>, <em>Java</em> and <em>JavaScript</em>.
+          </p>
+        </div>
+      </main>
     </div>
   );
 }

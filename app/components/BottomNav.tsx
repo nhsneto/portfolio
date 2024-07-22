bd50eed8e3de8b92e68cc4aa10dec8d8b3cec854
Nelson Neto <nhsneto@gmail.com>
2024-07-22 01:05:42 -0300

    Change bottom navigation items' styles according to the current page

--- app/components/BottomNav.tsx
+++ app/components/BottomNav.tsx
@@ -1,42 +1,57 @@
-import { FolderOpen, Home, Handyman } from "@/app/icons";
+"use client";
+
+import {
+  Home,
+  HomeFilled,
+  Handyman,
+  HandymanFilled,
+  FolderOpen,
+  FolderOpenFilled,
+} from "@/app/icons";
 import Link from "next/link";
 import { montserrat } from "@/app/fonts";
 import BottomNavItem from "@/app/components/BottomNavItem";
+import { usePathname } from "next/navigation";
 import styles from "./BottomNav.module.css";
 
 export default function BottomNav() {
+  const currentPathname = usePathname();
+  const isHome = currentPathname === "/";
+  const isSkills = currentPathname === "/skills";
+  const isProjects = currentPathname === "/projects";
+
   return (
     <div className={`${styles.container} ${montserrat.className}`}>
       <Link href="/">
         <BottomNavItem
-          icon={Home}
+          icon={isHome ? HomeFilled : Home}
           iconWidth="24px"
           iconHeight="24px"
           iconColor="var(--gray)"
           label="Home"
-          isActive={true}
+          isActive={isHome}
         />
       </Link>
 
       <Link href="/skills">
         <BottomNavItem
-          icon={Handyman}
+          icon={isSkills ? HandymanFilled : Handyman}
           iconWidth="24px"
           iconHeight="24px"
           iconColor="var(--gray)"
           label="Skills"
-          isActive={false}
+          isActive={isSkills}
         />
       </Link>
 
       <Link href="/projects">
         <BottomNavItem
-          icon={FolderOpen}
+          icon={isProjects ? FolderOpenFilled : FolderOpen}
           iconWidth="24px"
           iconHeight="24px"
           iconColor="var(--gray)"
           label="Projects"
-          isActive={false}
+          isActive={isProjects}
         />
       </Link>
     </div>

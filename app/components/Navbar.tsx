118ac29ed5bf41dbc62ac71b62217296f4b3d244
Nelson Neto <nhsneto@gmail.com>
2024-07-21 23:59:25 -0300

    Export type IconProps
    - Rename type 'Props' to the specific name 'IconProps'
    - Rename 'fill' field to 'color' for better context comprehension

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,10 +5,10 @@ export default function Navbar() {
   return (
     <nav className={styles.socialsContainer}>
       <a href="https://linkedin.com/in/nhsneto" target="_blank">
-        <Linkedin width="24" height="24" fill="var(--gray)" />
+        <Linkedin width="24" height="24" color="var(--gray)" />
       </a>
       <a href="https://github.com/nhsneto" target="_blank">
-        <Github width="27" height="27" fill="var(--gray)" />
+        <Github width="27" height="27" color="var(--gray)" />
       </a>
     </nav>
   );

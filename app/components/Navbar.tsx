ce2ea9bcb87a24dd95f0e1b49726e1638a4be4d9
Nelson Neto <nhsneto@gmail.com>
2024-07-23 22:21:43 -0300

    Use Next.js built-in Link component over html anchor tag on Navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,15 +1,16 @@
 import { Github, Linkedin } from "@/app/icons";
+import Link from "next/link";
 import styles from "./Navbar.module.css";
 
 export default function Navbar() {
   return (
     <nav className={styles.socialsContainer}>
-      <a href="https://linkedin.com/in/nhsneto" target="_blank">
+      <Link href="https://linkedin.com/in/nhsneto" target="_blank">
         <Linkedin width="24" height="24" color="var(--gray)" />
-      </a>
-      <a href="https://github.com/nhsneto" target="_blank">
+      </Link>
+      <Link href="https://github.com/nhsneto" target="_blank">
         <Github width="27" height="27" color="var(--gray)" />
-      </a>
+      </Link>
     </nav>
   );
 }

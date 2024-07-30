e92971a9d8af00bfc8ebc27c39f222179c42fe60
Nelson Neto <nhsneto@gmail.com>
2024-07-30 01:28:25 -0300

    Change nav bar link styles according to the current page

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,9 +1,16 @@
+"use client";
 import { Github, Linkedin } from "@/app/icons";
+import { usePathname } from "next/navigation";
 import Link from "next/link";
 import { montserrat } from "@/app/fonts";
 import styles from "./Navbar.module.css";
 
 export default function Navbar() {
+  const currentPathname = usePathname();
+  const isHomePage = currentPathname === "/";
+  const isSkillsPage = currentPathname === "/skills";
+  const isProjectsPage = currentPathname === "/projects";
+
   return (
     <>
       <nav className={styles.container}>
@@ -16,9 +23,17 @@ export default function Navbar() {
       </nav>
 
       <nav className={`${styles.container} ${styles.desktop} ${montserrat.className}`}>
-        <Link href="/">Home</Link>
-        <Link href="/skills">Skills</Link>
-        <Link href="/projects">Projects</Link>
+        <div className={styles.textLinks}>
+          <Link href="/" className={isHomePage ? styles.currentPage : ""}>
+            Home
+          </Link>
+          <Link href="/skills" className={isSkillsPage ? styles.currentPage : ""}>
+            Skills
+          </Link>
+          <Link href="/projects" className={isProjectsPage ? styles.currentPage : ""}>
+            Projects
+          </Link>
+        </div>
         <Link href="https://linkedin.com/in/nhsneto" target="_blank">
           <Linkedin width="24" height="24" color="var(--gray)" />
         </Link>

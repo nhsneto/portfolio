77aa5de98d5d17dae2063b60772aaa55e3296ab3
Nelson Neto <nhsneto@gmail.com>
2024-07-30 00:33:51 -0300

    Make Navbar component responsive

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,16 +1,31 @@
 import { Github, Linkedin } from "@/app/icons";
 import Link from "next/link";
+import { montserrat } from "@/app/fonts";
 import styles from "./Navbar.module.css";
 
 export default function Navbar() {
   return (
-    <nav className={styles.socialsContainer}>
-      <Link href="https://linkedin.com/in/nhsneto" target="_blank">
-        <Linkedin width="24" height="24" color="var(--gray)" />
-      </Link>
-      <Link href="https://github.com/nhsneto" target="_blank">
-        <Github width="27" height="27" color="var(--gray)" />
-      </Link>
-    </nav>
+    <>
+      <nav className={styles.container}>
+        <Link href="https://linkedin.com/in/nhsneto" target="_blank">
+          <Linkedin width="24" height="24" color="var(--gray)" />
+        </Link>
+        <Link href="https://github.com/nhsneto" target="_blank">
+          <Github width="27" height="27" color="var(--gray)" />
+        </Link>
+      </nav>
+
+      <nav className={`${styles.container} ${styles.desktop} ${montserrat.className}`}>
+        <Link href="/">Home</Link>
+        <Link href="/skills">Skills</Link>
+        <Link href="/projects">Projects</Link>
+        <Link href="https://linkedin.com/in/nhsneto" target="_blank">
+          <Linkedin width="24" height="24" color="var(--gray)" />
+        </Link>
+        <Link href="https://github.com/nhsneto" target="_blank">
+          <Github width="27" height="27" color="var(--gray)" />
+        </Link>
+      </nav>
+    </>
   );
 }

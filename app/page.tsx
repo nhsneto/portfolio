3d4399c6267b6f676a2a03b60adb3cab585aa68e
Nelson Neto <nhsneto@gmail.com>
2024-07-31 00:09:58 -0300

    Change home page's styles - Increase font weight of programming languages - Align text to the right - Remove breakpoint at 1280px

--- app/page.tsx
+++ app/page.tsx
@@ -6,16 +6,20 @@ export default function Home() {
   return (
     <Layout>
       <div className={styles.container}>
-        <h1 className={styles.title}>Nelson Neto</h1>
-        <p className={`${styles.subtitle} ${montserrat.className}`}>
-          Software Developer
-        </p>
-        <p className={`${styles.about} ${montserrat.className}`}>
-          Hi, I&apos;m a junior software developer based in Pernambuco (Brazil),
-          and a software development student at <abbr>IFPE</abbr> (Instituto
-          Federal de Pernambuco). I&apos;ve developed some projects using{" "}
-          <em>PHP</em>, <em>Java</em> and <em>JavaScript</em>.
-        </p>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Nelson Neto</h1>
+          <p className={`${styles.subtitle} ${montserrat.className}`}>
+            Software Developer
+          </p>
+          <p className={`${styles.about} ${montserrat.className}`}>
+            Hi, I&apos;m a junior software developer based in Pernambuco
+            (Brazil), and a software development student at <abbr>IFPE</abbr>{" "}
+            (Instituto Federal de Pernambuco). I&apos;ve developed some projects
+            using <em className={styles.em}>PHP,</em>{" "}
+            <em className={styles.em}>Java</em> and{" "}
+            <em className={styles.em}>JavaScript.</em>
+          </p>
+        </div>
       </div>
     </Layout>
   );

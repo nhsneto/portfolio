e2e308298d9c2e30afd45281e0c53e8eeeaf485d
Nelson Neto <nhsneto@gmail.com>
2025-05-31 01:45:22 -0300

    Remove subtitle and about sections

--- app/page.tsx
+++ app/page.tsx
@@ -8,17 +8,17 @@ export default function Home() {
       <div className={styles.container}>
         <div className={styles.content}>
           <h1 className={styles.title}>Nelson Neto</h1>
-          <p className={`${styles.subtitle} ${montserrat.className}`}>
+          {/* <p className={`${styles.subtitle} ${montserrat.className}`}>
             Software Developer
-          </p>
-          <p className={`${styles.about} ${montserrat.className}`}>
+          </p> */}
+          {/* <p className={`${styles.about} ${montserrat.className}`}>
             Hi, I&apos;m a junior software developer based in Pernambuco
             (Brazil), and a software development student at <abbr>IFPE</abbr>{" "}
             (Instituto Federal de Pernambuco). I&apos;ve developed some projects
             using <em className={styles.em}>PHP,</em>{" "}
             <em className={styles.em}>Java</em> and{" "}
             <em className={styles.em}>JavaScript.</em>
-          </p>
+          </p> */}
         </div>
       </div>
     </Layout>
